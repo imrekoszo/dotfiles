0559712d1995c94d547e4d3127b0d9affad64c58
Imre Kószó <imrekoszo@users.noreply.github.com>
2017-11-06 12:29:56 +0000

    add current configuration

---
+++ .lein/profiles.d/user.clj
@@ -0,0 +1,8 @@
+{:plugins
+ [[lein-ancient "0.6.10"]
+  [lein-pprint "1.1.1"]
+  [test2junit "1.2.2"]
+  ;[lein-kibit "0.1.6-SNAPSHOT"]
+  [lein-kibit "0.1.5"]
+  [jonase/eastwood "0.2.4"]
+  [lein-dotenv "RELEASE"]]}
